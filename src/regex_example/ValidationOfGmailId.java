5338aceb7fe98360b576743d81cc9746c50a4b48
Amir Khan <Amirk8161@gmail.com>
2019-01-01 14:21:46 +0530

    first commit
    Validate gmail Id

---
+++ src/regex_example/ValidationOfGmailId.java
@@ -0,0 +1,19 @@
+package regex_example;
+
+import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class ValidationOfGmailId {
+
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+		String email = scanner.next();
+		scanner.close();
+		Pattern pattern = Pattern.compile("([a-zA-Z]+)([a-zA-Z0-9_.])*@gmail.com");
+		Matcher matcher = pattern.matcher(email);
+		System.out.println(matcher.find() && email.equals(matcher.group()));
+		System.out.println(matcher.group());
+	}
+
+}
